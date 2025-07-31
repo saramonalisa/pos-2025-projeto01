3f06b669ce791cf722c9cf075a8e61feb6dfe90b
Sara Monalisa <Saramonaliscs@gmail.com>
2025-07-31 11:31:44 -0300

    Adicionando rotas

--- app.py
+++ app.py
@@ -21,3 +21,31 @@ oauth.register(
     authorize_url='https://suap.ifrn.edu.br/o/authorize/',
     fetch_token=lambda: session.get('suap_token')
 )
+
+@app.route('/')
+def index():
+    if 'suap_token' in session:
+        meus_dados = oauth.suap.get('v2/minhas-informacoes/meus-dados')
+        return render_template('user.html', user_data=meus_dados.json())
+    else:
+        return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    redirect_uri = url_for('auth', _external=True)
+    print(redirect_uri)
+    return oauth.suap.authorize_redirect(redirect_uri)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('suap_token', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/login/authorized')
+def auth():
+    token = oauth.suap.authorize_access_token()
+    session['suap_token'] = token
+    return redirect(url_for('index'))
